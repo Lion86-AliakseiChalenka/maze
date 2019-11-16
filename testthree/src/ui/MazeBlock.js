2e586a652cf2a3506db652dd380be1a9aede35a9
Lion86 <klanchalenk@gmail.com>
2019-11-16 14:14:05 +0300

    version 1.1 . Descktop version

--- testthree/src/ui/MazeBlock.js
+++ testthree/src/ui/MazeBlock.js
@@ -1,35 +1,82 @@
 import React from 'react';
 import {connect} from 'react-redux'
-import MazeCellStart from "./MazeCellStart.js";
-import MazeCell from "./MazeCell.js";
+import {setGameCondition, setAccessToButton, getMessage} from "./../redux/reducer.js";import MazeCell from "./MazeCell.js";
+import {Alert} from 'antd';
+import 'antd/dist/antd.css';
 
 function MazeBlock(props) {
+    const {
+        mazeField, startPoint, finishPoint,
+        isMessageDisplayed, setGameCondition,
+        gameResult, isGameFinished, cellWidth, cellHeight,
+        setAccessToButton, getMessage
+    } = props
     const mazeStyle = {
-        width: (props.mazeField.length === 9 && '210px') ||
-        (props.mazeField.length === 25 && '350px') ||
-        (props.mazeField.length === 49 && '490px'),
+        width: (mazeField.length === 9 && `${2 * 14 + 14 + cellWidth * 3}px`) ||
+        (mazeField.length === 25 && `${4 * 14 + 14 + cellWidth * 5}px`) ||
+        (mazeField.length === 49 && `${6 * 14 + 14 + cellWidth * 7}px`) ||
+        (mazeField.length === 81 && `${8 * 14 + 14 + cellWidth * 9}px`),
         display: 'flex',
         flexWrap: 'wrap',
-        backgroundColor: '#669999'
+        backgroundColor: '#f2f4f5'
     }
-    const {mazeField, startPoint, finishPoint} = props
+
     const maze = mazeField.map((el, index) => {
-        if (startPoint && el.join('') === startPoint.join(''))
-            return <MazeCellStart key={index}
-                                  position={el}
-                                  finishPoint={finishPoint}/>
-        else return <MazeCell key={index}
+       return <MazeCell key={index}
                               position={el}
-                              finishPoint={finishPoint}/>
+                              startPoint={startPoint}
+                              finishPoint={finishPoint}
+                              setGameCondition={setGameCondition}
+                              gameResult={gameResult}
+                              isGameFinished={isGameFinished}
+                              cellWidth={cellWidth}
+                              cellHeight={cellHeight}/>
     })
+    const onClose = () => {
+        setGameCondition(true, null)
+        setAccessToButton(false)
+    }
+    const onWarningClose = () => {
+        getMessage(false)
+    }
 
     return (
         <>
-            {mazeField.length > 0 ?
+            {(mazeField.length > 0
+                &&
                 <div style={mazeStyle}>
                     {maze}
-                </div>
-                : <Message/>
+                </div>)
+            || (isMessageDisplayed
+                &&
+                <Alert style={{position: 'absolute', margin: '0 auto'}}
+                       closable
+                       onClose={onWarningClose}
+                       message="Warning"
+                       description="Please, set maze size"
+                       type="warning"
+                       showIcon/>)}
+            {isGameFinished && gameResult === 'win'
+            &&
+            <Alert
+                style={{position: 'absolute', margin: '0 auto'}}
+                closable
+                onClose={onClose}
+                message="Success"
+                description="Congratulations! You are winner."
+                type="success"
+                showIcon/>}
+            {isGameFinished && gameResult === 'lose'
+            &&
+            <Alert
+                style={{position: 'absolute', margin: '0 auto'}}
+                closable
+                onClose={onClose}
+                message="Failed"
+                description="Sorry, you are looser."
+                type="error"
+                showIcon
+            />
             }
         </>
     );
@@ -38,8 +85,14 @@ function MazeBlock(props) {
 const mstp = (state) => {
     return {
         mazeField: state.mazeField,
+        mazeSize: state.mazeSize,
         startPoint: state.startPoint,
-        finishPoint: state.finishPoint
+        finishPoint: state.finishPoint,
+        isMessageDisplayed: state.isMessageDisplayed,
+        isGameFinished: state.isGameFinished,
+        gameResult: state.gameResult,
+        cellWidth: state.cellWidth,
+        cellHeight: state.cellHeight
     }
 }
-export default connect(mstp, {})(MazeBlock);
+export default connect(mstp, {setGameCondition, setAccessToButton, getMessage})(MazeBlock);
